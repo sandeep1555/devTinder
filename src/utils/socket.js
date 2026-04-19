0d9d49cd07c30c6bf474505a97ebae224b54b33e
sandeep1555 <sandeepdasari614@gmail.com>
2026-04-20 01:13:14 +0530

    socket error resolved

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -12,7 +12,7 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FE_ORIGIN,
     },
   });
 
